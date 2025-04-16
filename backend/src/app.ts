79a9e05c1a6d0f965699ef8289940d4a52564da3
lemuesousa <lemuel124.ldsp@gmail.com>
2025-04-16 08:29:38 -0300

    feat: create seed loader and configure cors origins

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,12 @@ const PORT = 3000;
 
 app.use(express.json());
 
-const allowedOrigin = ["http://localhost:5173", "http://127.0.0.1:5173"];
+const allowedOrigin = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "http://localhost:5174",
+  "http://127.0.0.1:5174",
+];
 
 app.use(cors({ origin: allowedOrigin }));
 
